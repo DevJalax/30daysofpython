bc5b767bbe8324f5a418b25cf26bc808a796cb84
DevJalax <75425954+DevJalax@users.noreply.github.com>
2020-12-04 14:59:09 +0530

    Add files via upload

---
+++ day5_part1.py
@@ -0,0 +1,8 @@
+i1 = input("Enter num 1")
+i2 = input("Enter num 2")
+
+def func(i1,i2):
+      print("Addition of 2 nos is",i1+i2)
+      print("Sub of 2 nos is",i1-i2)
+      print("Mul of 2 nos is",i1*i2)
+      print("Div of 2 nos is",i1/i2)
---
+++ day5_part2.py
@@ -0,0 +1,3 @@
+def covid():
+      name = input("Enter your username")
+      temp = 98
