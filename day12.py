e05064146fbe63407426b50c797ad69cfe82a1a3
DevJalax <75425954+DevJalax@users.noreply.github.com>
2020-12-05 12:15:17 +0530

    Update day12.py

--- day12.py
+++ day12.py
@@ -1,19 +1,10 @@
+import sqlite3
 import json
-#dictionary
-print(json.dumps({"Name":"john","doe":20}))
-#list
-print(json.dumps(["Amala","Dany"]))
-#string
-print(json.dumps(("Carolina","Matt")))
-#tuple
-print(json.dumps("Elena"))
-#integer
-print(json.dumps(80))
-#float
-print(json.dumps(25.1))
-#True
-print(json.dumps(True))
-#False
-print(json.dumps(False))
-#None
-print(json.dumps(None))
+conn = sqlite3.connect('test.db')
+c = conn.cursor();
+c.execute("CREATE TABLE DT(id varchar(9),data json)")
+c.execute("insert into DT values(?,?)",(1,json.dumps({"Name":"john","doe":20})),(2,json.dumps(["Amala","Dany"])),(3,json.dumps(("Carolina","Matt"))),(4,json.dumps("Elena")),(5,json.dumps(80)),(6,json.dumps(25.1)),(7,json.dumps(True)),(8,json.dumps(False)),(9,json.dumps(None)))
+c.execute("SELECT * FROM DT")
+rows = c.fetchall()
+for row in rows:
+    print(row)
